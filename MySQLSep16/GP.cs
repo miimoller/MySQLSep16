271aa464045cc1b033c31653eb9e77ee86f98542
Rohan Bhakta <1040125@student.auhsd.us>
2022-10-27 19:12:28 -0700

    10/27home

--- MySQLSep16/GP.cs
+++ MySQLSep16/GP.cs
@@ -1,4 +1,5 @@
-﻿using Org.BouncyCastle.Crypto;
+﻿using Org.BouncyCastle.Asn1.X509.SigI;
+using Org.BouncyCastle.Crypto;
 using System;
 using System.Collections.Generic;
 using System.Drawing.Printing;
@@ -11,18 +12,19 @@ namespace MySQLSep16
 {
     internal class GP
     {
-        public string car { get; set; }
+        public string[] car { get; set; }
         public string[] gasStation { get; set; }
-        public string sponser { get; set; }
+        public string[] sponser { get; set; }
+        public string[] person { get; set; }
 
         public GameSpace[,] gameBoard { get; set; }
-
+        Random random = new Random();
 
         public void fillBoard()
         {
             GameSpace emptySpace = new GameSpace
             {
-                symbol = '0',
+                symbol = ' ',
                 symbolType = GameSpace.space.empty
             };
             for (int i = 0; i < gameBoard.GetLength(0); i++)
@@ -45,65 +47,98 @@ namespace MySQLSep16
             gasStation[4] = "_|oo []|_||l";
             gasStation[5] = "|_______|/";
             gasStation[6] = "|_______|";
+
+            sponser=new string[7];
+            sponser[0] = "____________";
+            sponser[1] = "|          |";
+            sponser[2] = "|McDonalds |";
+            sponser[3] = "|__[Food]__| ______";
+            sponser[4] = "     ||  ___|__|   |";
+            sponser[5] = "     ||  |[_]|     |";
+            sponser[6] = "     ||  |   |  [] |";
+
+            person = new string[3];
+            person[0] = " O";
+            person[1] = "/|\\";
+            person[2] = "/ \\";
+
+            car = new string[5];
+            car[0] = "/^^\\";
+            car[1] = "*--*";
+            car[2] = "|  |";
+            car[3] = "*--*";
+            car[4] = "\\__/";
+
+
         }
         public void makeGame()
         {
 
             makeModels();
-            Random random = new Random();
-            
-
-           
-            int length = random.Next(500, 1000);
-
-
-            gameBoard = new GameSpace[length, 100];
+ 
+            gameBoard = new GameSpace[random.Next(500, 1000), 100];
 
             fillBoard();
 
-            
-                Console.WriteLine((gameBoard.GetLength(0)));
-                
-                for (int i = 0; i <( gameBoard.GetLength(1)/20)-1; i++)
-                {
+            randomizeModels(gasStation, 12, 50, 60, GameSpace.space.gas);
+            randomizeModels(sponser, 20, 80, 90, GameSpace.space.sponser);
+            randomizeModels(car, 5, 10, 15, GameSpace.space.car);
+            randomizeModels(person, 4, 20, 30, GameSpace.space.person);
 
-                    int dy = random.Next(0, 20);
-                    int x = random.Next(0, gameBoard.GetLength(0)-15);
 
-                    Console.WriteLine("x:"+x+"y:"+ (i * 10));
-                    printGasModel(x, i * 10 );
-
-                }
-            
             showScreen();
         }
-        public void printGasModel(int x, int y)
+        public void randomizeModels(string[] model, int rad, int min, int max, GameSpace.space type)
+        {
+            for (int i = 0; i < gameBoard.GetLength(0); i = i + random.Next(min, max))
+            {
+
+                // int dy = random.Next(0, 20);
+                int x = random.Next(0, gameBoard.GetLength(1));
+
+
+                printModel(model, x, i, rad, type);
+
+            }
+        }
+        public bool checkRadius(int x, int y, int r)
+        {
+            for (int i = x - r; i < x + r; i++)
+            {
+                for (int j = y - r; j < y + r; j++)
+                {
+                    if (i<0 || j<0 || x+r+1>gameBoard.GetLength(1) || y+r+1>gameBoard.GetLength(0) || gameBoard[j,i].symbol!=' ')
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public void printModel(string[] s, int x, int y, int r, GameSpace.space type)
         {
 
-            if (x<gameBoard.GetLength(1)&&y<gameBoard.GetLength(1))
+            if (checkRadius(x,y,r))
             {
-                for (int i = 0; i < gasStation.Length; i++)
+                for (int i = 0; i < s.Length; i++)
                 {
-                    char[] line = gasStation[i].ToCharArray();
+                    char[] line = s[i].ToCharArray();
                     for (int j = 0; j < line.Length; j++)
                     {
 
                         GameSpace temp = new GameSpace
                         {
                             symbol = line[j],
-                            symbolType = GameSpace.space.gas
+                            symbolType = type
                         };
 
                         gameBoard[y + i, x + j] = temp;
                     }
 
                 }
+                Console.WriteLine("x:" + x + "y:" + y);
             }
         }
-
-
-
-
         public void showScreen()
         {
             for (int i = 0; i < gameBoard.GetLength(0); i++)
--- MySQLSep16/GameSpace.cs
+++ MySQLSep16/GameSpace.cs
@@ -8,7 +8,7 @@ namespace MySQLSep16
 {
     internal class GameSpace
     {
-        public enum space { empty, gas, sponser, car, obstacle}
+        public enum space { empty, gas, sponser, car, person}
         public char symbol { get; set; }
         public space symbolType { get; set; }
     }
